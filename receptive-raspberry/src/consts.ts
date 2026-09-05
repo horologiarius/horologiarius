d31f1cea08aa69d74ab2431b3404d9156947f38f
horologiarius <contact@horologiarius.com>
2026-09-05 11:38:53 -0400

    Fix syntax error in SITE_TITLE declaration

--- receptive-raspberry/src/consts.ts
+++ receptive-raspberry/src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'The Watchmaker;
+export const SITE_TITLE = 'The Watchmaker';
 export const SITE_DESCRIPTION = 'Horologiarius: essays about cosmology, philosophy and the underlying architecture of things.';
